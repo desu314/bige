77af221c23f1ca01379a6c04b9a59f0435110022
desu314 <1162903270@qq.com>
2018-08-15 17:13:35 +0800

    create project

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://shop.tearea.com/admin/');
+define('HTTP_CATALOG', 'http://shop.tearea.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://shop.tearea.com/admin/');
+define('HTTPS_CATALOG', 'http://shop.tearea.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/wwwroot/bige/upload/admin/');
+define('DIR_SYSTEM', '/home/wwwroot/bige/upload/system/');
+define('DIR_IMAGE', '/home/wwwroot/bige/upload/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home/wwwroot/bige/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '0okm(IJN8uhb');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'bg_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
